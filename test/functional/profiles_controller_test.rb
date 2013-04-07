ac7e08717d096427a6178f6e0e0cd8aa85b4a047
william kenny <william.r.kenny@gmail.com>
2013-04-07 16:52:13 +0600

    added scoped status updates

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,27 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:kenny).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "renders a 404 page if the user isn't found" do
+    get :show, id: "does not exsist"
+    assert_response :not_found
+  end
+
+  test "that variables are assigned on a successful profile viewing page" do
+    get :show, id: users(:kenny).profile_name
+    assert assigns(:user)
+    assert_not_empty assigns(:statuses)
+  end
+ 
+  test "only show the correct users statuses" do
+    get :show, id: users(:kenny).profile_name
+    assigns(:statuses).each do |status|
+    	assert_equal users(:kenny), status.user 
+    end
   end
 
 end
